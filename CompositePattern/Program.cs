c3288c06dd9e628c7efbae0064bc778107a8d7f2
Albert <albert.capdevila@buclenet.com>
2018-01-27 21:21:50 +0100

    Primer ejemplo de aplicación del patrón en el cálculo de costes de un conjunto de productos

--- CompositePattern/Program.cs
+++ CompositePattern/Program.cs
@@ -9,8 +9,13 @@ namespace CompositePattern
         {
             // Ejemplo para la definición del patrón
             PintarUnArbol();
-        }
 
+            // Ejemplo real de costes
+            CalcularCostesDeUnProductoFormadoPorOtrosProductos();
+
+            Console.ReadLine();
+        }
+        
         private static void PintarUnArbol()
         {
             var troncoArbol = new Rama();
@@ -33,9 +38,8 @@ namespace CompositePattern
 
             troncoArbol.Pintar(1);
 
-            Console.ReadLine();
+            
         }
-
 
         public abstract class ComponenteArbol
         {
@@ -85,6 +89,128 @@ namespace CompositePattern
             public override void Pintar(int nivel)
             {
                 Console.WriteLine(new String('-', nivel) + " hoja");
+            }
+        }
+
+        private static void CalcularCostesDeUnProductoFormadoPorOtrosProductos()
+        {
+            var conjuntoB = CrearConjuntoB();
+            
+            Console.WriteLine("Coste de " + conjuntoB.Nombre + ":");
+            Console.WriteLine(conjuntoB.CalcularCoste());
+        }
+
+        private static Conjunto CrearConjuntoB()
+        {
+            var referenciaB = new Referencia("B", 4);
+            var conjuntoB = new Conjunto(referenciaB);
+
+            var referenciaB1 = new Referencia("B1", 1);
+            var piezaB1 = new Pieza(referenciaB1);
+
+            var referenciaB2 = new Referencia("B2", 2);
+            var conjuntoB2 = new Conjunto(referenciaB2);
+
+            var referenciaB21 = new Referencia("B21", 1);
+            var piezaB21 = new Pieza(referenciaB21);
+
+            var referenciaB22 = new Referencia("B22", 2);
+            var piezaB22 = new Pieza(referenciaB22);
+
+            conjuntoB2.Añadir(4, piezaB21);
+            conjuntoB2.Añadir(2, piezaB22);
+
+            conjuntoB.Añadir(5, piezaB1);
+            conjuntoB.Añadir(3, conjuntoB2);
+
+            return conjuntoB;
+        }
+
+        public class Referencia
+        {
+            public Referencia(string nombre, decimal coste)
+            {
+                if (string.IsNullOrEmpty(nombre))
+                {
+                    throw new ArgumentException();
+                }
+
+                Nombre = nombre;
+                Coste = coste;
+            }
+            public string Nombre { get;  }
+            public decimal Coste { get; }
+        }
+
+        public abstract class ComponenteProducto
+        {
+            public virtual void Añadir(int cantidad, ComponenteProducto componente)
+            {
+                throw new NotImplementedException();
+            }
+
+            public virtual void Quitar(ComponenteProducto componente)
+            {
+                throw new NotImplementedException();
+            }
+            public abstract decimal CalcularCoste();
+        }
+
+        public class Conjunto : ComponenteProducto
+        {
+            private readonly Referencia _referencia;
+            private readonly List<ComponenteProducto> _subComponentes;
+
+
+            public string Nombre
+            {
+                get { return _referencia.Nombre;  }
+            }
+
+            public Conjunto(Referencia referencia)
+            {
+                _referencia = referencia;
+                _subComponentes = new List<ComponenteProducto>();
+            }
+            
+            public override void Añadir(int cantidad, ComponenteProducto componente)
+            {
+                for (int i = 0; i < cantidad; i++)
+                {
+                    _subComponentes.Add(componente);
+                }
+            }
+
+            public override void Quitar(ComponenteProducto componente)
+            {
+                _subComponentes.Remove(componente);
+            }
+            
+            public override decimal CalcularCoste()
+            {
+                decimal coste = _referencia.Coste;
+
+                foreach (var componenteProducto in _subComponentes)
+                {
+                    coste = coste + componenteProducto.CalcularCoste();
+                }
+
+                return coste;
+            }
+        }
+
+        public class Pieza : ComponenteProducto
+        {
+            private readonly Referencia _referencia;
+
+            public Pieza(Referencia referencia)
+            {
+                _referencia = referencia;
+            }
+
+            public override decimal CalcularCoste()
+            {
+                return _referencia.Coste;
             }
         }
     }
